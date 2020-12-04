b0d3cbe435cff438835cec081406c983499e56d1
DruidUnderscoreBoy <61715465+DruidUnderscoreBoy@users.noreply.github.com>
2020-12-04 02:09:03 -0500

    A whole slew of shit for the playtest

---
+++ Project Hmong/Assets/PlayerHealthOld.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthOld : MonoBehaviour
+{
+    [AddComponentMenu("Health")]
+    public int playerHp = 3;
+    public int damage = 1;
+    bool isDead;
+
+    // Start is called before the first frame update
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+    void OnParticleCollision(GameObject other)
+    {
+        playerHp = playerHp -  damage;
+        Debug.Log("I'm hit");
+        if (playerHp <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        if (isDead) return;
+        isDead = true;
+        
+    }
+}
